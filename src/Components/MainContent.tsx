06392f05e5fcb16aa13842d443b0dcbf6c1c45a3
ManiacD16 <msd161918@gmail.com>
2024-11-09 19:35:05 +0530

    Rank

--- src/Components/MainContent.tsx
+++ src/Components/MainContent.tsx
@@ -747,7 +747,7 @@ const EcommerceReferralPage = () => {
             <h2 className="text-3xl font-bold text-center mb-5 text-gray-800 dark:text-gray-200">
               Yield Packages
             </h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
               {yieldPackages.map((pkg, index) => (
                 <div
                   key={index}
